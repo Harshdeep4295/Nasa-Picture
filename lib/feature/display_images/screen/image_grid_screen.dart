53a83c50ff71f4ec6fb4f86d94cf6e79d8942a57
harshdeep.singh <Harshdeep.singh@pasarpolis.com>
2021-04-18 17:58:44 +0530

    images are displayed.

--- lib/feature/display_images/screen/image_grid_screen.dart
+++ lib/feature/display_images/screen/image_grid_screen.dart
@@ -1,4 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:nasa_picture/core/injector/injector.dart';
+import 'package:nasa_picture/core/utils/app_utils.dart';
+import 'package:nasa_picture/feature/display_images/bloc/bloc.dart';
+import 'package:nasa_picture/feature/display_images/bloc/bloc_event.dart';
+import 'package:nasa_picture/feature/display_images/bloc/bloc_state.dart';
+import 'package:nasa_picture/feature/display_images/domain/entity/image_model.dart';
 
 class ImageGridScreen extends StatefulWidget {
   @override
@@ -6,10 +13,52 @@ class ImageGridScreen extends StatefulWidget {
 }
 
 class _ImageGridScreenState extends State<ImageGridScreen> {
+  late ImageDisplayBloc _imageDisplayBloc;
+
+  @override
+  void initState() {
+    super.initState();
+    _imageDisplayBloc = sl.get<ImageDisplayBloc>();
+  }
+
+  Widget _body(ImageScreenState state) {
+    return GridView.count(
+      crossAxisCount: 2,
+      children: AppUtilities.map(
+        list: state.imageDataList!.images,
+        handler: (index, ImageModel imageDetails) {
+          return InkWell(
+            onTap: () => _imageDisplayBloc.add(
+              ImageClickEvent(index),
+            ),
+            child: Hero(
+              transitionOnUserGestures: true,
+              tag: "$index",
+              child: Image.network(
+                imageDetails.url,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Welcome To The Nasa"),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Nasa Pictures"),
+      ),
+      body: BlocProvider(
+        create: (context) => _imageDisplayBloc,
+        child: BlocBuilder(
+          bloc: _imageDisplayBloc,
+          builder: (context, ImageScreenState state) {
+            return _body(state);
+          },
+        ),
+      ),
     );
   }
 }
